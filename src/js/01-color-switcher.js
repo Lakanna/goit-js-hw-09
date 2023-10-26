09de132e9eadbbbd9c8f1efd97610e90d00a5911
Lakanna <annalakus@ukr.net>
2023-10-26 20:58:48 +0100

    upd task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
 
+const elements = {
+    btnStart: document.querySelector("button[data-start]"),
+    btnStop: document.querySelector("button[data-stop]"),
+    body: document.querySelector('body'),
+};
+
+let timerId = null;
+
+elements.btnStop.disabled = true;
+
+elements.btnStart.addEventListener('click', handlerStart);
+elements.btnStop.addEventListener('click', handlerStop);
+
+
+function handlerStart() {
+    if (!elements.btnStop.disabled) {
+    return;
+    } 
+    timerId = setInterval(getColor, 1000);
+    elements.btnStop.disabled = false;
+};
+
+function getColor() {
+   
+    const currentColor = getRandomHexColor();
+    console.log(currentColor);
+    elements.body.style.backgroundColor = currentColor;
+    elements.btnStart.disabled = true;
+};
+
+function handlerStop() {
+
+    clearInterval(timerId);
+
+    elements.btnStart.disabled = false;
+    elements.btnStop.disabled = true;
+};
+
+function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    };
